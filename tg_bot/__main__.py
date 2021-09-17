5a9dacbda5ae571932a9abb88bba56a47025e88e
KING XV <85796566+Judson-web@users.noreply.github.com>
2021-09-17 22:34:39 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -35,7 +35,7 @@ PM_START_TEXT = """
 """
 
 HELP_STRINGS = """
-Hey {},! My name is *{}*.
+Hey There 🙂,! My name is *{}*.
 I'm a modular group management bot with a few fun extras! Have a look at the following for an idea of some of \
 the things I can help you with.
 *Main* commands available:
