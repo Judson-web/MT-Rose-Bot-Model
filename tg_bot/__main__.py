9f910066564f2f56d9210c47cadb289c531d968c
KING XV <85796566+Judson-web@users.noreply.github.com>
2021-09-17 22:25:43 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -51,10 +51,10 @@ And the following:
 """.format(dispatcher.bot.first_name, "" if not ALLOW_EXCL else "\nAll commands can either be used with / or !.\n")
 
 DONATE_STRING = DONATE_STRING = """Heya, glad to hear you want to donate!
-It took lots of work for [my creator](t.me/professorforreveal12) to get me to where I am now, and every donation helps \
+It took lots of work for [my editor](t.me/VAMPIRE_KING_NO_1) to get me to where I am now, and every donation helps \
 motivate him to make me even better. All the donation money will go to a better VPS to host me, and/or beer \
 (see his bio!). He's just a poor student, so every little helps!
-There are one way of paying him; [Conact Him Through Pm](t.me/professorforreveal12)."""
+There are one way of paying him; [Contact THrough Pm](t.me/VAMPIRE_KING_NO_1)."""
 
 IMPORTED = {}
 MIGRATEABLE = []
